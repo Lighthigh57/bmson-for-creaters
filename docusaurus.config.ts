01e134609cd9bab81a6ee04bb0cedd58ad85132e
Lighthigh57 <78249745+Lighthigh57@users.noreply.github.com>
2024-09-25 13:25:45 +0900

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'Lighthigh57', // Usually your GitHub org/user name.
   projectName: 'bmson-for-creaters', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
